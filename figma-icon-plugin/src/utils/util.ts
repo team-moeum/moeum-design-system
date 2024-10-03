7f6330d5063de3626b0b20950dddd46f2919cf1f
seungdeok <akk9132@naver.com>
2024-10-03 20:24:05 +0900

    refactor: change package manager

--- figma-icon-plugin/src/utils/util.ts
+++
@@ -1,9 +0,0 @@
-export function isEmptyObj(obj)  {
-  if (!obj) return true;
-  if(obj.constructor === Object
-     && Object.keys(obj).length === 0)  {
-    return true;
-  }
-  
-  return false;
-}
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import { main } from "./build.js";
-
-main();
