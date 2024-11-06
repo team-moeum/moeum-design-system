a03cf1ae668131a6cb9f793c87f4e872cdc20897
seungdeok <akk9132@naver.com>
2024-11-06 22:53:43 +0900

    refactor(@fotcamp/components): add TextField on components

---
+++ packages/fotcamp-components/src/components/TextField/TextField.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import cx from "classnames";
+import { isIcon } from "../../shared/icon";
+import { TextFieldProps, TextFieldSize } from "./TextField.type";
+
+function TextFieldSide({
+  size,
+  children,
+}: {
+  size: TextFieldSize;
+  children: React.ReactNode;
+}) {
+  if (isIcon(children)) {
+    return <>To-Do Icon</>;
+    // return <Icon source={children} size={size} />;
+  }
+
+  return <>{children}</>;
+}
+
+export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
+  (
+    {
+      className,
+      size = "medium",
+      variant = "surface",
+      color,
+      radius,
+      type = "text",
+      disabled,
+      error,
+      leftContent,
+      rightContent,
+      ...props
+    },
+    ref
+  ) => {
+    const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+      props.onFocus?.(e);
+    };
+
+    const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+      props.onBlur?.(e);
+    };
+
+    return (
+      <div
+        data-fotcamp-component="Textfield"
+        className={cx(
+          "textfield-container",
+          {
+            [`textfield-container--size-${size}`]: size,
+            [`textfield-container--variant-${variant}`]: variant,
+            [`textfield-container--radius-${radius}`]: radius,
+            "textfield-container--disabled": disabled,
+            "textfield-container--error": error,
+          },
+          className
+        )}
+      >
+        {leftContent && (
+          <TextFieldSide size={size}>{leftContent}</TextFieldSide>
+        )}
+        <input
+          type={type}
+          disabled={disabled}
+          ref={ref}
+          className="textfield"
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          style={{ color }}
+          {...props}
+        />
+        {rightContent && (
+          <TextFieldSide size={size}>{rightContent}</TextFieldSide>
+        )}
+      </div>
+    );
+  }
+);
