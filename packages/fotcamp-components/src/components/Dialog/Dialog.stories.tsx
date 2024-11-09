79b95008a18fc5b7f756ee4c03a778820553d146
widse <wngud6012@naver.com>
2024-11-10 03:45:27 +0900

    feat: Dialog

---
+++ packages/fotcamp-components/src/components/Dialog/Dialog.stories.tsx
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Dialog } from "./Dialog";
+import { useOverlay } from "../../hooks/useOverlay/useOverlay";
+import { Button } from "../Button";
+import { OverlayProvider } from "../../hooks/useOverlay/OverlayProvider";
+import { Flex } from "../layout/Flex";
+import { DialogRadius, DialogSize } from "./Dialog.type";
+
+const meta = {
+  title: "Components/Dialog",
+  component: Dialog,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      control: "select",
+      options: Object.values(DialogSize),
+    },
+    radius: {
+      control: "select",
+      options: Object.values(DialogRadius),
+    },
+    modal: {
+      control: "boolean",
+    },
+  },
+  decorators: [
+    (Story) => (
+      <OverlayProvider>
+        <Story />
+      </OverlayProvider>
+    ),
+  ],
+} satisfies Meta<typeof Dialog>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const DialogContentA = () => {
+  return (
+    <Flex height={500} p={20} align="center" justify="center">
+      Dialog
+    </Flex>
+  );
+};
+
+export const Primary: Story = {
+  args: {
+    size: "medium",
+    radius: "medium",
+  },
+  render: (args) => {
+    const overlay = useOverlay();
+
+    const openOverlay = () => {
+      overlay.open(({ isOpen, close }) => (
+        <Dialog
+          {...args}
+          open={isOpen}
+          onClose={() => {
+            close();
+          }}
+          children={<DialogContentA />}
+        />
+      ));
+    };
+
+    return <Button onClick={openOverlay}>Open</Button>;
+  },
+};
---
+++ packages/fotcamp-components/src/components/Dialog/Dialog.type.ts
@@ -0,0 +1,28 @@
+export const DialogSize = {
+  SMALL: "small",
+  MEDIUM: "medium",
+  LARGE: "large",
+} as const;
+
+export const DialogRadius = {
+  NONE: "none",
+  SMALL: "small",
+  MEDIUM: "medium",
+  LARGE: "large",
+} as const;
+
+type ValueOf<T> = T[keyof T];
+
+export type DialogSize = ValueOf<typeof DialogSize>;
+export type DialogRadius = ValueOf<typeof DialogRadius>;
+
+export interface DialogProps {
+  open?: boolean;
+  onClose?: () => void;
+  modal?: boolean;
+  zIndex?: number;
+  radius?: DialogSize;
+  size?: DialogSize;
+  className?: string;
+  children?: React.ReactNode;
+}
