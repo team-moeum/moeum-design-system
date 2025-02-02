4a814249e8b8e0bea4d43b3e096bc62f9f79cab5
seungdeok <akk9132@naver.com>
2025-02-02 19:32:56 +0900

    docs(@moeum/components): change storybook layout

--- packages/moeum-components/src/components/Button/Button.stories.tsx
+++ packages/moeum-components/src/components/Button/Button.stories.tsx
@@ -68,11 +68,89 @@ export const ButtonSNS: Story = {
           <Text> Google로 로그인</Text>
         </Flex>
       </Button>
+      <Button type="base" size="lg" color="#391B1B" backgroundColor="#FAE300">
+        <Flex direction="row" align="center" justify="center" gap={8}>
+          <Kakao />
+          <Text>Kakao로 로그인</Text>
+        </Flex>
+      </Button>
+      <Button type="base" size="lg" color="#FFFFFF" backgroundColor="#191B1C">
+        <Flex direction="row" align="center" justify="center" gap={8}>
+          <Apple />
+          <Text>Apple로 로그인</Text>
+        </Flex>
+      </Button>
+      <Button type="base" size="lg" color="#191B1C" backgroundColor="#FFFFFF">
+        <Flex direction="row" align="center" justify="center" gap={8}>
+          <Google />
+          <Text> Google로 로그인</Text>
+        </Flex>
+      </Button>
     </div>
   )
 };
 
-export const ButtonSolid: Story = {
+export const ButtonLarge: Story = {
+  args: {},
+  render: () => (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 16,
+        alignItems: "center"
+      }}
+    >
+      <Button type="solid-green" size="lg">
+        버튼(active, pressed)
+      </Button>
+      <Button type="solid-red" size="lg">
+        버튼(active, pressed)
+      </Button>
+      <Button type="outline-green" size="lg">
+        버튼(active, pressed)
+      </Button>
+      <Button type="outline-gray" size="lg">
+        버튼(active, pressed)
+      </Button>
+      <Button type="text-none" size="lg">
+        버튼(active, pressed)
+      </Button>
+      <Button type="solid-green" size="lg" disabled>
+        버튼(inactive)
+      </Button>
+      <Button type="solid-red" size="lg" disabled>
+        버튼(inactive)
+      </Button>
+      <Button type="outline-green" size="lg" disabled>
+        버튼(inactive)
+      </Button>
+      <Button type="outline-gray" size="lg" disabled>
+        버튼(inactive)
+      </Button>
+      <Button type="text-none" size="lg" disabled>
+        버튼(inactive)
+      </Button>
+      <Button type="solid-green" size="lg" loading>
+        버튼(loading)
+      </Button>
+      <Button type="solid-red" size="lg" loading>
+        버튼(loading)
+      </Button>
+      <Button type="outline-green" size="lg" loading>
+        버튼(loading)
+      </Button>
+      <Button type="outline-gray" size="lg" loading>
+        버튼(loading)
+      </Button>
+      <Button type="text-none" size="lg" loading>
+        버튼(loading)
+      </Button>
+    </div>
+  )
+};
+
+export const ButtonMedium: Story = {
   args: {},
   render: () => (
     <div
@@ -84,79 +162,49 @@ export const ButtonSolid: Story = {
       }}
     >
       <Button type="solid-green" size="md">
-        solid-green
-      </Button>
-      <Button type="solid-green" size="md" disabled>
-        solid-green disabled
-      </Button>
-      <Button type="solid-green" size="md" loading>
-        solid-green loading
+        버튼(active, pressed)
       </Button>
       <Button type="solid-red" size="md">
-        solid-red
+        버튼(active, pressed)
       </Button>
-      <Button type="solid-red" size="md" disabled>
-        solid-red disabled
-      </Button>
-      <Button type="solid-red" size="md" loading>
-        solid-red loading
-      </Button>
-    </div>
-  )
-};
-
-export const ButtonOutline: Story = {
-  args: {},
-  render: () => (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 16,
-        alignItems: "center"
-      }}
-    >
       <Button type="outline-green" size="md">
-        outline-green
-      </Button>
-      <Button type="outline-green" size="md" disabled>
-        outline-green disabled
-      </Button>
-      <Button type="outline-green" size="md" loading>
-        outline-green loading
+        버튼(active, pressed)
       </Button>
       <Button type="outline-gray" size="md">
-        outline-gray
+        버튼(active, pressed)
+      </Button>
+      <Button type="text-none" size="md">
+        버튼(active, pressed)
+      </Button>
+      <Button type="solid-green" size="md" disabled>
+        버튼(inactive)
+      </Button>
+      <Button type="solid-red" size="md" disabled>
+        버튼(inactive)
+      </Button>
+      <Button type="outline-green" size="md" disabled>
+        버튼(inactive)
       </Button>
       <Button type="outline-gray" size="md" disabled>
-        outline-gray disabled
-      </Button>
-      <Button type="outline-gray" size="md" loading>
-        outline-gray loading
-      </Button>
-    </div>
-  )
-};
-
-export const ButtonText: Story = {
-  args: {},
-  render: () => (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 16,
-        alignItems: "center"
-      }}
-    >
-      <Button type="text-none" size="md">
-        text-none
+        버튼(inactive)
       </Button>
       <Button type="text-none" size="md" disabled>
-        text-none disabled
+        버튼(inactive)
+      </Button>
+      <Button type="solid-green" size="md" loading>
+        버튼(loading)
+      </Button>
+      <Button type="solid-red" size="md" loading>
+        버튼(loading)
+      </Button>
+      <Button type="outline-green" size="md" loading>
+        버튼(loading)
+      </Button>
+      <Button type="outline-gray" size="md" loading>
+        버튼(loading)
       </Button>
       <Button type="text-none" size="md" loading>
-        text-none loading
+        버튼(loading)
       </Button>
     </div>
   )
